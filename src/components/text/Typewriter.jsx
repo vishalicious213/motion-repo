a34c6ca423962e9f45183def44cc0666762ac323
vishalicious213 <vishalicious213@gmail.com>
2025-08-24 15:27:49 -0700

    updated Typewriter

--- src/components/text/Typewriter.jsx
+++ src/components/text/Typewriter.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { motion } from "motion/react"
 
 const Typewriter = () => {
     return (
-        <div>Typewriter</div>
+        <motion.div
+            style={{ overflow: "hidden", whiteSpace: "nowrap"}}
+            initial={{ width: 0 }}
+            animate={{ width: "100%" }}
+            transition={{
+                duration: 2,
+                ease: "easeInOut"
+            }}
+        >
+            Typewriter
+        </motion.div>
     )
 }
 
