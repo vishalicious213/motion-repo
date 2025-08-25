d9a331a2b014d85f226921864e21d1be55e6ee0f
vishalicious213 <vishalicious213@gmail.com>
2025-08-25 12:41:46 -0700

    updated ModalAnimation

--- src/components/layout/ModalAnimation.jsx
+++ src/components/layout/ModalAnimation.jsx
@@ -1,9 +1,30 @@
 import { useState } from "react"
 import { motion, AnimatePresence } from "motion/react"
 
-const ModalAnimation = () => {
+const ModalAnimation = ({ onClose }) => {
     return (
-        <div>ModalAnimation</div>
+        <motion.div
+            className="overlay"
+            onClick={onClose}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+        >
+            <motion.div
+                className="modal-card"
+                onClick={ (e) => e.stopPropagation() }
+                initial={{ scale: 0.9, opacity: 0 }}
+                animate={{ scale: 1, opacity: 1 }}
+                exit={{ scale: 0.9, opacity: 0 }}
+                transition={{
+                    type: "spring",
+                    stiffness: 400,
+                    damping: 25
+                }}
+            >
+
+            </motion.div>
+        </motion.div>
     )
 }
 
