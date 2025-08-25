0f4681b8e212e1a4d06503f14eab9b77b20f3b84
vishalicious213 <vishalicious213@gmail.com>
2025-08-25 10:14:11 -0700

    updated SizeShift

--- src/components/layout/SizeShift.jsx
+++ src/components/layout/SizeShift.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import { useState } from "react"
+import { motion } from "motion/react"
 
 const SizeShift = () => {
+    const [size, setSize] = useState(100)
+
+    const toggleSize = () => {
+        setSize(size === 100 ? 150 : 100)
+    }
+
     return (
-        <div>SizeShift</div>
+        <div>
+            <button onClick={toggleSize}>Toggle size</button>
+            <motion.div
+                style={{
+                    width: size,
+                    height: size,
+                    background: "lightblue",
+                    margin: "20px"
+                }}
+            >
+
+            </motion.div>
+        </div>
     )
 }
 
