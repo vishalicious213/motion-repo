63d9a7d2806ef2b5b9877e9ae9f21a3b591bb80c
vishalicious213 <vishalicious213@gmail.com>
2025-08-25 15:35:36 -0700

    updated /pages/PTAbout.jsx

--- src/pages/PTAbout.jsx
+++ src/pages/PTAbout.jsx
@@ -1,9 +1,13 @@
 import { motion, AnimatePresence } from "motion/react"
 
 const PTAbout = () => {
-  return (
-    <div>PTAbout</div>
-  )
+    return (
+        <div>
+            <h1>Welcome to the About Page</h1>
+
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem voluptatibus dolore officiis cupiditate non doloribus labore, ipsam dignissimos! Praesentium aliquid accusantium expedita natus exercitationem facilis quam aperiam debitis veniam libero!</p>
+        </div>
+    )
 }
 
 export default PTAbout
