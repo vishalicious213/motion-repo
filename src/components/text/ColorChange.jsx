001757bc6b60970ae49d0807942ffa7ffe5d8724
vishalicious213 <vishalicious213@gmail.com>
2025-08-25 09:01:02 -0700

    updated ColorChange

--- src/components/text/ColorChange.jsx
+++ src/components/text/ColorChange.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import { motion } from "motion/react"
 
 const ColorChange = () => {
     return (
-        <div>ColorChange</div>
+        <motion.div
+            initial={{ color: "#333" }}
+            animate={{ color: ["#6a9bd1", "#4c6ef5", "#1a3e96", "#ff0000"] }}
+            transition={{ 
+                duration: 2,
+                repeat: Infinity
+            }}
+        >
+            Color Change
+        </motion.div>
     )
 }
 
