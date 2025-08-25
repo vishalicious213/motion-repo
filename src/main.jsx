307b3638d2fb0b8fadd9541e91d93c68030231cc
vishalicious213 <vishalicious213@gmail.com>
2025-08-25 15:38:44 -0700

    main: added routes for PTHome & PTAbout

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router'
+import PTHome from './pages/PTHome.jsx'
+import PTAbout from './pages/PTAbout.jsx'
 import './index.css'
 import App from './App.jsx'
 
@@ -9,6 +11,8 @@ createRoot(document.getElementById('root')).render(
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<App />} />
+				<Route path="/pthome" element={<PTHome />} />
+				<Route path="/ptabout" element={<PTAbout />} />
 			</Routes>
 		</BrowserRouter>
 	</StrictMode>,
