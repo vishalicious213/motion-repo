346d2c7555bb4221e234401c8884b86419c5f3e2
vishalicious213 <vishalicious213@gmail.com>
2025-08-26 02:53:29 -0700

    drafted /components/ColorChangePulse.jsx

---
+++ src/components/text/ColorChangePulse.jsx
@@ -0,0 +1,20 @@
+import { motion } from "motion/react"
+
+const ColorChange = () => {
+    return (
+        <motion.div
+            className="gradient-text-pulse"
+            initial={{ backgroundSize: "100% 100%" }}
+            animate={{ backgroundSize: ["100% 100%", "300% 300%", "100% 100%"] }}
+            transition={{
+                duration: 6,
+                repeat: Infinity,
+                ease: "easeInOut"
+            }}
+        >
+            Color Change
+        </motion.div>
+    )
+}
+
+export default ColorChange
